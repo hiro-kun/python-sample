cd6ffb7dcc1833c43b53d53e3911a850da00201d
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-03-08 00:03:33 +0900

    fix クラス化

--- image_resize/src/image_resize.py
+++ image_resize/src/image_resize.py
@@ -12,10 +12,21 @@ from PIL import Image, ImageFilter
 import glob
 import os
 
-files = glob.glob('../image/base/*.jpg')
+class Image_Resize:
 
-for file in files:
-    img = Image.open(file)
-    img_resize_lanczos = img.resize((512, 512), Image.LANCZOS)
-    gray_img = img_resize_lanczos.convert('L')
-    gray_img.save('../image/out/' + os.path.basename(file))
+    @staticmethod
+    def get_base_images():
+        return glob.glob('../image/base/*.jpg')
+
+    @staticmethod
+    def image_resize(image_file_path):
+        img = Image.open(image_file_path)
+        img_resize_lanczos = img.resize((512, 512), Image.LANCZOS)
+        gray_img = img_resize_lanczos.convert('L')
+        gray_img.save('../image/out/' + os.path.basename(image_file_path))
+
+
+
+images = Image_Resize.get_base_images()
+for image in images:
+    Image_Resize.image_resize(image)
