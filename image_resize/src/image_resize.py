cdab61cf8ae0ee8668afe7a5146f1cbb351c4cf7
ono-hiroshi <hiroshi_ono_1989@yahoo.co.jp>
2018-03-12 23:08:34 +0900

    fix ディレクトリパス定数化

--- image_resize/src/image_resize.py
+++ image_resize/src/image_resize.py
@@ -8,19 +8,21 @@ from joblib import Parallel, delayed
 import glob
 import os
 
+IMG_BASE_DIR = '../image/base/*.jpg'
+IMG_OUT_DIR = '../image/out/'
+
 class Image_Resize:
 
     @staticmethod
     def get_base_images():
-        return glob.glob('../image/base/*.jpg')
+        return glob.glob(IMG_BASE_DIR)
 
     @staticmethod
     def image_resize(image_file_path):
         img = Image.open(image_file_path)
         resize_image = img.resize((512, 512), Image.LANCZOS)
         gray_img = resize_image.convert('L')
-        gray_img.save('../image/out/' + os.path.basename(image_file_path))
-
+        gray_img.save(IMG_OUT_DIR + os.path.basename(image_file_path))
 
 
 images = Image_Resize.get_base_images()
